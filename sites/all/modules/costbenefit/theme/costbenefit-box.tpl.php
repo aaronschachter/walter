524593e5cf65319f692fef7ecd61baff944944fd
Aaron Schachter <aaronschachter@gmail.com>
2014-04-22 01:11:55 -0400

    Adds type radios, helper functions

--- sites/all/modules/costbenefit/theme/costbenefit-box.tpl.php
+++ sites/all/modules/costbenefit/theme/costbenefit-box.tpl.php
@@ -1,9 +1,16 @@
+<?php 
+/*
+@todo: Add total counts.  Also breakdown by short term long term.
+*/
+?>
 <div>
 <h3>Box <?php print $vars['box']; ?>
-  <ul>
+  <table>
   <?php foreach ($vars['items'] as $item): ?>
-    <li><?php print l($item['title'], 'cb/' . $vars['cb_id'] . '/' . $item['cb_item_id']); ?></li>
+    <tr>
+    <td><?php print l($item['title'], 'cb/' . $vars['cb_id'] . '/' . $item['cb_item_id']); ?></td>
+    </tr>
   <?php endforeach; ?>
-  </ul>
+  </table>
   <?php print $vars['link']; ?>
 </div>

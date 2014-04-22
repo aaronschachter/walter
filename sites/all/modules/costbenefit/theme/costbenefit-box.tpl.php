3b0516818e5035b3b8a5a11734e51e274fdfc7c8
Aaron Schachter <aaronschachter@gmail.com>
2014-04-22 00:01:32 -0400

    Adds delete form, changes URL paths to cb/[cb_id]/[cb_item_id] pattern

--- sites/all/modules/costbenefit/theme/costbenefit-box.tpl.php
+++ sites/all/modules/costbenefit/theme/costbenefit-box.tpl.php
@@ -2,7 +2,7 @@
 <h3>Box <?php print $vars['box']; ?>
   <ul>
   <?php foreach ($vars['items'] as $item): ?>
-    <li><?php print l($item['title'], 'cb-item/' . $item['cb_item_id']); ?></li>
+    <li><?php print l($item['title'], 'cb/' . $vars['cb_id'] . '/' . $item['cb_item_id']); ?></li>
   <?php endforeach; ?>
   </ul>
   <?php print $vars['link']; ?>

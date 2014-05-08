7a65d8a4adf6aa91b1f409c08dd596018705bac1
Aaron Schachter <aaronschachter@gmail.com>
2014-05-08 00:40:26 -0400

    Adds cost benefit edit form in modal

--- sites/all/themes/bootstrap_float/templates/costbenefit.tpl.php
+++ sites/all/themes/bootstrap_float/templates/costbenefit.tpl.php
@@ -1,5 +1,9 @@
-<h1 class="page-header"><?php print $title ; ?></h1>
-<div class="pull-right"><?php print l('Edit', 'cb/' . arg(1) . '/edit'); ?></div>
+<h1 class="page-header"><?php print $title; ?>
+<button class="btn btn-tertiary pull-right" data-toggle="modal" data-target=".edit-title-modal">
+   <span class="glyphicon glyphicon-pencil"></span>
+</button>
+</h1>
+
 
 <div class="row">
   <div class="col-xs-6"><?php print render($content['boxes'][1]); ?></div>
@@ -10,4 +14,21 @@
   <div class="col-xs-6"><?php print render($content['boxes'][3]); ?></div>
   <div class="col-xs-6"><?php print render($content['boxes'][4]); ?></div>
 </div>
+<div>
 
+
+
+<!-- Edit title modal -->
+<div class="modal fade edit-title-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Edit</h4>
+      </div>
+      <div class="modal-body">
+        <p><?php print render($content['edit_form']); ?></p>
+      </div>
+    </div>
+  </div>
+</div>

0292fc791ada471bbbc3ec6c8f10cbf98eb700bc
Aaron Schachter <aaronschachter@gmail.com>
2014-05-08 21:31:53 -0400

    Adds a costbenefit-front tpl for login

---
+++ sites/all/modules/costbenefit/theme/costbenefit-front.tpl.php
@@ -0,0 +1,15 @@
+Walter is an app for creating and storing a Cost Benefit Analysis.
+
+<div class="row">
+  <div class="col-xs-6">
+    <ul>
+      <li>Enter a substance or habit you want to stop.</li>
+      <li>Add the pros and cons of doing the habit, and not doing the habit.</li>
+      <li>Keep it in your pocket.</li>
+    </ul>
+  </div>
+  <div class="col-xs-6">
+    <h3>Login</h3>
+    <?php print render(drupal_get_form('user_login')); ?>
+  </div>
+</div>
--- sites/all/themes/bootstrap_float/template.php
+++ sites/all/themes/bootstrap_float/template.php
@@ -5,6 +5,7 @@
  * template.php
  */
 function bootstrap_float_preprocess_page(&$variables) {
+  $variables['show_title'] = TRUE;
   if (arg(0) == 'cb') {
     $variables['show_title'] = FALSE;
   }

db10dc5063f5e1ff00fb871aee431f0a46f0233f
Aaron Schachter <aaronschachter@gmail.com>
2014-05-08 20:47:08 -0400

    Adds add-cb-form in modal to cb-list

--- sites/all/modules/costbenefit/theme/costbenefit-list.tpl.php
+++ sites/all/modules/costbenefit/theme/costbenefit-list.tpl.php
@@ -1,4 +1,24 @@
 <div>
+
+  <button class="btn btn-tertiary pull-right" data-toggle="modal" data-target=".add-cb-modal">
+    <span class="glyphicon glyphicon-plus"></span>
+  </button>
+
   <?php print $list; ?>
-  <?php print render($add_form); ?>
+
+</div>
+
+<!-- Add costbenefit modal -->
+<div class="modal fade add-cb-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Add new</h4>
+      </div>
+      <div class="modal-body">
+        <p><?php print render($add_form); ?></p>
+      </div>
+    </div>
+  </div>
 </div>

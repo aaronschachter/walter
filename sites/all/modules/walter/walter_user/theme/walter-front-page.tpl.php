af1b7b9820fc881084b14e42a794630f3035ecad
Aaron Schachter <aaronschachter@gmail.com>
2014-05-09 22:04:48 -0700

    Render login form in modal

--- sites/all/modules/walter/walter_user/theme/walter-front-page.tpl.php
+++ sites/all/modules/walter/walter_user/theme/walter-front-page.tpl.php
@@ -15,17 +15,34 @@
 continuing or discontinuing the behavior.<sup>1</sup></p>
   </div>
   <div class="col-xs-6">
-    <div class="well">
-      <?php print render($login_form); ?>
-    </div>
-    <div class="well">
-      <h3>Sound useful? <small>Sign up, it's free.</small></h3>
+    <div class="">
+      <h3>Sound useful? <small>Try it out, it's free.</small></h3>
       <?php print render($register_form); ?>
       <hr>
-      <p class="lead">Already have an account? Sign in.</p>
+      <p class="lead text-center">Already have an account?
+        <button class="btn" data-toggle="modal" data-target=".login-form-modal">
+          Sign in
+        </button>
+      </p>
     </div>
   </div>
 </div>
 <div class="front-footer">
   <p class="small"><sup>1</sup>http://www.smartrecovery.org/resources/library/Tools_and_Homework/Facilitators_Handout/CBA.pdf</p>
 </div>
+
+<!-- Login form modal -->
+<div class="modal fade login-form-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Sign In</h4>
+      </div>
+      <div class="modal-body">
+        <p><?php print render($login_form); ?></p>
+      </div>
+    </div>
+  </div>
+</div>
+

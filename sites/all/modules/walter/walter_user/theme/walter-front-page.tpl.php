20d3878fd749d6a12578422f40fcd3badfe596ff
Aaron Schachter <aaronschachter@gmail.com>
2014-05-09 21:36:53 -0700

    Move front_page into walter_user module

---
+++ sites/all/modules/walter/walter_user/theme/walter-front-page.tpl.php
@@ -0,0 +1,26 @@
+<div class="row">
+  <div class="col-xs-6">
+    <header role="banner" id="page-header">
+      <p class="lead">Walter is an app for building motivation to change.</p>
+    </header>
+    <ul>
+      <li>Enter a substance or habit you want to stop.</li>
+      <li>List the pros and cons of DOING and NOT DOING it.</li>
+      <li>Keep them in your pocket (because they're on your phone).</li>
+    </ul>
+    <p>
+    <p>The Cost-Benefit Analysis is a tool used in SMART Recovery.  It is a method for exploring the short-term vs. long-term benefits associated with 
+continuing or discontinuing the behavior.</p>
+  </div>
+  <div class="col-xs-6">
+    <div class="well">
+      <?php print render(drupal_get_form('user_login_block')); ?>
+    </div>
+    <div class="well">
+      <h3>Sound useful? <small>Sign up, it's free.</small></h3>
+      <?php print render(drupal_get_form('user_register_form')); ?>
+      <hr>
+      <p class="lead">Already have an account? Sign in.</p>
+    </div>
+  </div>
+</div>

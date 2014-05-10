00505d77336d6b1de5a813e61bfaea342d5f362b
Aaron Schachter <aaronschachter@gmail.com>
2014-05-09 21:49:26 -0700

    Pass login and register forms as vars to front_page tpl

--- sites/all/modules/walter/walter_user/theme/walter-front-page.tpl.php
+++ sites/all/modules/walter/walter_user/theme/walter-front-page.tpl.php
@@ -3,24 +3,29 @@
     <header role="banner" id="page-header">
       <p class="lead">Walter is an app for building motivation to change.</p>
     </header>
+    <div class="jumbotron">
     <ul>
       <li>Enter a substance or habit you want to stop.</li>
       <li>List the pros and cons of DOING and NOT DOING it.</li>
       <li>Keep them in your pocket (because they're on your phone).</li>
     </ul>
+  </div>
     <p>
-    <p>The Cost-Benefit Analysis is a tool used in SMART Recovery.  It is a method for exploring the short-term vs. long-term benefits associated with 
-continuing or discontinuing the behavior.</p>
+    <p>The Cost-Benefit Analysis is a tool used in <a href="http://smartrecovery.org" target="_blank">SMART Recovery</a>.  It is a method for exploring the short-term vs. long-term benefits associated with 
+continuing or discontinuing the behavior.<sup>1</sup></p>
   </div>
   <div class="col-xs-6">
     <div class="well">
-      <?php print render(drupal_get_form('user_login_block')); ?>
+      <?php print render($login_form); ?>
     </div>
     <div class="well">
       <h3>Sound useful? <small>Sign up, it's free.</small></h3>
-      <?php print render(drupal_get_form('user_register_form')); ?>
+      <?php print render($register_form); ?>
       <hr>
       <p class="lead">Already have an account? Sign in.</p>
     </div>
   </div>
+</div>
+<div class="front-footer">
+  <p class="small"><sup>1</sup>http://www.smartrecovery.org/resources/library/Tools_and_Homework/Facilitators_Handout/CBA.pdf</p>
 </div>

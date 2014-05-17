3bb8805cc6db916304f8406fe97f3d94bc3697cd
Aaron Schachter <aaronschachter@gmail.com>
2014-05-17 11:26:37 -0400

    Adds comments

--- sites/all/themes/bootstrap_float/template.php
+++ sites/all/themes/bootstrap_float/template.php
@@ -4,13 +4,23 @@
  * @file
  * template.php
  */
+
+/**
+ * Implements hook_preprocess_page().
+ */
 function bootstrap_float_preprocess_page(&$variables) {
+  // Hide page titles on cb/* pages.
   $variables['show_title'] = TRUE;
   if (arg(0) == 'cb') {
     $variables['show_title'] = FALSE;
   }
-}
+  // Adds Bootstrap Validator files.
+  drupal_add_css(drupal_get_path('theme', 'bootstrap_float') . '/dist/css/bootstrapValidator.css');
+  drupal_add_js(drupal_get_path('theme', 'bootstrap_float') . '/dist/css/bootstrapValidator.js');}
 
+/**
+ * Implements theme_form_required_marker().
+ */
 function bootstrap_float_form_required_marker($variables) {
   return;
 }

6a7b624c78cbb45733c8126c65703a2110010501
Aaron Schachter <aaronschachter@gmail.com>
2014-05-17 19:58:53 -0400

    ugh even more copy changes

--- sites/all/modules/walter/walter_user/theme/walter-front-page.tpl.php
+++ sites/all/modules/walter/walter_user/theme/walter-front-page.tpl.php
@@ -7,11 +7,11 @@
   <div class="col-md-6">
     <p class="lead">List short-term and long-term benefits or continuing or discontinuing it.</li>
     <p>The lists together make up a <strong>Cost Benefit Analysis</strong>, a tool used in <a href="http://smartrecovery.org" target="_blank">SMART Recovery</a>.
-    <p>Login to see or add to your Cost Benefit anytime.</p>
+    <p>Sign up for <strong>Walter</strong> to create and maintain your own private Cost Benefit Analysis.  Oh, and it's free!</p>
   </div>
   <div class="col-md-6">
     <div class="">
-      <h3>Sound useful? <small>Try it out, it's free.</small></h3>
+      <h4>Sign Up</h4>
       <?php print render($register_form); ?>
     </div>
   </div>

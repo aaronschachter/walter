96bf0f44d991d40e53bd11172fc6324721cd314b
Aaron Schachter <aaronschachter@gmail.com>
2014-05-17 18:35:41 -0400

    Cleaner homepage

--- sites/all/modules/walter/walter_user/theme/walter-front-page.tpl.php
+++ sites/all/modules/walter/walter_user/theme/walter-front-page.tpl.php
@@ -1,20 +1,18 @@
 <div class="row">
-  <div class="col-md-6">
+  <div class="col-md-7">
     <header role="banner" id="page-header">
       <p class="lead">Walter is an app for building motivation to change.</p>
     </header>
-    <div class="jumbotron">
+
     <ul>
       <li>Enter a substance or habit you want to stop.</li>
       <li>List the pros and cons of DOING and NOT DOING it.</li>
       <li>Keep them in your pocket (because they're on your phone).</li>
     </ul>
-  </div>
-    <p>
     <p>The Cost-Benefit Analysis is a tool used in <a href="http://smartrecovery.org" target="_blank">SMART Recovery</a>.  It is a method for exploring the short-term vs. long-term benefits associated with
 continuing or discontinuing the behavior.<sup>1</sup></p>
   </div>
-  <div class="col-md-6">
+  <div class="col-md-5">
     <div class="">
       <h3>Sound useful? <small>Try it out, it's free.</small></h3>
       <?php print render($register_form); ?>

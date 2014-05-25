997c129921baa16b1aa70ca04a30d5b68e3724fe
Aaron Schachter <aaronschachter@gmail.com>
2014-05-25 11:05:03 -0400

    Lots of homepage copy changes. Needs a node badly

--- sites/all/modules/walter/walter_user/theme/walter-front-page.tpl.php
+++ sites/all/modules/walter/walter_user/theme/walter-front-page.tpl.php
@@ -1,15 +1,24 @@
 <header role="banner" id="page-header" class="jumbotron">
   <h1>Walter helps you change.</h1>
-  <p>Pick a substance or activity that you want to quit.  Or start.</p>
+  <p>It's a web app to help you quit an activity or substance.</p>
 </header>
 
 <div class="row">
-  <div class="col-md-6">
-    <p class="lead">List short-term and long-term benefits or continuing of discontinuing it.</li>
-    <p>The lists together make up a <strong>Cost Benefit Analysis</strong>, a tool used in <a href="http://smartrecovery.org" target="_blank">SMART Recovery</a>.
-    <p>Sign up for <strong>Walter</strong> to create and maintain your own private Cost Benefit Analysis.  Oh, and it's free!</p>
+  <div class="col-md-8">
+    <ol>
+      <li class="lead">List the pros and cons of doing/using it.</li>
+      <li class="lead">Next, list the pros and cons of NOT doing/using it.</li>
+      <li class="lead">Keep them on your phone via Walter.</li>
+    </ol>
+    <p>The lists together make up a <strong>Cost Benefit Analysis</strong>, a tool used in <a href="http://smartrecovery.org" target="_blank">SMART Recovery</a> to explore the short-term and long-term benefits of continuing/discontinuing.
+    <p>The next time you feel the urge to indulge, open up Walter and read through your Cost Benefit Analysis. Unfortunately, there are no results guaranteed, but it might just help.</p>
+    <hr>
+    <p class="lead">It's free.</p>
+    <p>This first version of Walter allows you to access your Cost Benefit Analysis from the web at anytime.</p>
+    <p>iPhone and Android apps are in development, but not available yet.</p>
+    <p>Sign up for <strong>Walter</strong> to create and maintain your own private Cost Benefit Analysis.</p>
   </div>
-  <div class="col-md-6">
+  <div class="col-md-4">
     <div class="">
       <h4>Sign Up</h4>
       <?php print render($register_form); ?>

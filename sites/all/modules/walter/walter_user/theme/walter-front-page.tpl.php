bf3d8b5cd8f6f14d8d7c3108ddb6c10531d5ecc5
Aaron Schachter <aaronschachter@gmail.com>
2014-05-25 11:18:26 -0400

    Adds well div to make the list pop

--- sites/all/modules/walter/walter_user/theme/walter-front-page.tpl.php
+++ sites/all/modules/walter/walter_user/theme/walter-front-page.tpl.php
@@ -10,9 +10,10 @@
       <li class="lead">Next, list the pros and cons of NOT doing/using it.</li>
       <li class="lead">Keep them on your phone via Walter.</li>
     </ol>
+    <div class="well">
     <p>The lists together make up a <strong>Cost Benefit Analysis</strong>, a tool used in <a href="http://smartrecovery.org" target="_blank">SMART Recovery</a> to explore the short-term and long-term benefits of continuing/discontinuing.
     <p>The next time you feel the urge to indulge, open up Walter and read through your Cost Benefit Analysis. Unfortunately, there are no results guaranteed, but it might just help.</p>
-    <hr>
+    </div>
     <p class="lead">It's free.</p>
     <p>This first version of Walter allows you to access your Cost Benefit Analysis from the web at anytime.</p>
     <p>iPhone and Android apps are in development, but not available yet.</p>

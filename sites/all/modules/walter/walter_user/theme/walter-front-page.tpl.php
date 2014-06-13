62606f30052b544d90044d6146ef7cf5aa84de6b
Aaron Schachter <aaronschachter@gmail.com>
2014-06-12 22:18:55 -0400

    Copy changes

--- sites/all/modules/walter/walter_user/theme/walter-front-page.tpl.php
+++ sites/all/modules/walter/walter_user/theme/walter-front-page.tpl.php
@@ -10,9 +10,6 @@
       <li class="lead">Next, list the pros and cons of NOT doing/using it.</li>
       <li class="lead">Keep them on your phone via Walter.</li>
     </ol>
-    <hr />
-    <p>The lists together make up a <strong>Cost Benefit Analysis</strong>, a tool used in <a href="http://smartrecovery.org" target="_blank">SMART Recovery</a> to explore the short-term and long-term
-      benefits of doing/using.</p>
   </div>
   <div class="col-md-4">
     <div class="well">
